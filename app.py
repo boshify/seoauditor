a6d169a395576a3f5cd5aeb5f05851df7a1b5c63
boshify <140970186+boshify@users.noreply.github.com>
2023-10-01 13:09:36 -0600

    Update app.py

--- app.py
+++ app.py
@@ -32,14 +32,110 @@ def get_gpt_insights(content, content_type):
     return response.choices[0].message['content'].strip()
 
 def TT(soup):
-    # ... [Your TT function's implementation remains unchanged]
+    title_tag = soup.find('title')
+    result = {
+        "title": title_tag.text if title_tag else None,
+        "message": "",
+        "what_it_is": "The title tag provides a brief summary of the content of the page and is displayed in search engine results and browser tabs.",
+        "how_to_fix": "",
+        "audit_name": "Title Audit"
+    }
+
+    if not title_tag:
+        result["message"] = "Fail: Title tag is missing."
+        result["how_to_fix"] = "Add a title tag to the head section of your HTML."
+    elif len(title_tag.text) > 60:
+        result["message"] = f"Fail: Title tag is too long ({len(title_tag.text)} characters)."
+        result["how_to_fix"] = "Reduce the length of the title tag to 60 characters or fewer."
+    elif len(title_tag.text) < 10:
+        result["message"] = f"Fail: Title tag is too short ({len(title_tag.text)} characters)."
+        result["how_to_fix"] = "Increase the length of the title tag to at least 10 characters."
+    else:
+        result["message"] = "Pass: Title tag is within the recommended length."
+
+    return result
 
 def MD(soup):
-    # ... [Your MD function's implementation remains unchanged]
+    meta_description = soup.find('meta', attrs={"name": "description"})
+    result = {
+        "description": meta_description['content'] if meta_description else None,
+        "message": "",
+        "what_it_is": "The meta description provides a brief summary of the content of the page and is displayed in search engine results.",
+        "how_to_fix": "",
+        "audit_name": "Meta Description Audit"
+    }
+
+    if not meta_description or not meta_description.get('content'):
+        result["message"] = "Fail: Meta description is missing."
+        result["how_to_fix"] = "Add a meta description tag to the head section of your HTML with a relevant description of the page content."
+    elif len(meta_description['content']) > 160:
+        result["message"] = f"Fail: Meta description is too long ({len(meta_description['content'])} characters)."
+        result["how_to_fix"] = "Reduce the length of the meta description to 160 characters or fewer."
+    elif len(meta_description['content']) < 50:
+        result["message"] = f"Fail: Meta description is too short ({len(meta_description['content'])} characters)."
+        result["how_to_fix"] = "Increase the length of the meta description to at least 50 characters."
+    else:
+        result["message"] = "Pass: Meta description is within the recommended length."
+
+    return result
 
 def IL(url):
-    # Here's where we properly indent the IL function's body
-    # ... [Your IL function's implementation remains unchanged]
+    # [Your IL function's implementation from the new version]
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    link_elements = soup.find_all(['a', 'img'], recursive=True)
+    
+    error_messages, broken_links, very_long_links, access_errors, resource_as_page_links = [], [], [], [], []
+    progress_bar = st.progress(0)
+    progress_text = st.empty()
+    total_links = len(link_elements)
+
+    for index, element in enumerate(link_elements):
+        link_url = element.get('href' if element.name == 'a' else 'src')
+        if not link_url:
+            continue
+        if len(link_url) > 200:
+            very_long_links.append(f"Very long URL: {link_url}")
+        try:
+            r = requests.get(link_url, allow_redirects=True, timeout=5)
+            if r.status_code >= 400:
+                broken_links.append(f"Broken link (status {r.status_code}): {link_url}")
+        except requests.RequestException as e:
+            access_errors.append(f"Error accessing link ({e}): {link_url}")
+
+        if element.name == 'img' and element.parent.name == 'a':
+            resource_as_page_links.append(f"Resource img with URL {link_url} is formatted as a page link.")
+
+        progress_bar.progress((index + 1) / total_links)
+        progress_text.text(PROGRESS_MESSAGES[index % len(PROGRESS_MESSAGES)])
+
+    progress_bar.empty()
+    progress_text.empty()
+
+    if len(link_elements) > 100:
+        error_messages.append("This page has too many on-page links.")
+
+    grouped_errors = []
+    if access_errors:
+        grouped_errors.append('\n'.join(access_errors))
+    if broken_links:
+        grouped_errors.append('\n'.join(broken_links))
+    if very_long_links:
+        grouped_errors.append('\n'.join(very_long_links))
+    if resource_as_page_links:
+        grouped_errors.append('\n'.join(resource_as_page_links))
+    if error_messages:
+        grouped_errors.append('\n'.join(error_messages))
+
+    result = {
+        "message": "\n\n".join(grouped_errors),
+        "what_it_is": "Linking checks for the page.",
+        "how_to_fix": "Each link issue is categorized. Review the specific issue category for recommendations.",
+        "audit_name": "Linking Audit"
+    }
+
+    return result
 
 # Streamlit App
 st.title("Single Page SEO Auditor")
