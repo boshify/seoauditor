8a25622bf1cc49dac0b0fe13eceb8f9113ca1631
boshify <140970186+boshify@users.noreply.github.com>
2023-10-01 13:27:09 -0600

    Update app.py

--- app.py
+++ app.py
@@ -35,6 +35,7 @@ def TT(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     title_tag = soup.find('title')
+    
     result = {
         "title": title_tag.text if title_tag else None,
         "message": "",
@@ -61,6 +62,7 @@ def MD(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     meta_description = soup.find('meta', attrs={"name": "description"})
+    
     result = {
         "description": meta_description['content'] if meta_description else None,
         "message": "",
@@ -106,15 +108,72 @@ def internal_link_analysis_gpt(error_messages, broken_links, very_long_links, ac
     return response.choices[0].message['content'].strip()
 
 def IL(url):
-    # [Implementation of the IL function as provided in the previous responses, with the modification to use internal_link_analysis_gpt()]
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    # Filtering links that are part of the main content (within paragraph, div, and image tags)
+    link_elements = soup.find_all(['a', 'img'], recursive=True)
+    
+    error_messages = []
+    broken_links = []
+    very_long_links = []
+    access_errors = []
+    resource_as_page_links = []
+    
+    progress_bar = st.progress(0)
+    progress_text = st.empty()
+    total_links = len(link_elements)
 
-# Streamlit App
+    for index, element in enumerate(link_elements):
+        link_url = element.get('href' if element.name == 'a' else 'src')
+        
+        # Check if URL is valid and not empty
+        if not link_url:
+            continue
+
+        # Check for long URLs
+        if len(link_url) > 200:
+            very_long_links.append(f"Very long URL: {link_url}")
+
+        try:
+            r = requests.get(link_url, allow_redirects=True, timeout=5)
+
+            # Check for broken links
+            if r.status_code >= 400:
+                broken_links.append(f"Potential broken link (status {r.status_code}): {link_url}")
+
+        except requests.RequestException as e:
+            access_errors.append(f"Error accessing link ({e}): {link_url}")
+
+        # Check for resources formatted as page links
+        if element.name == 'img' and element.parent.name == 'a':
+            resource_as_page_links.append(f"Resource img with URL {link_url} is formatted as a page link.")
+
+        # Update the progress bar with rotating messages
+        progress_bar.progress((index + 1) / total_links)
+        progress_text.text(PROGRESS_MESSAGES[index % len(PROGRESS_MESSAGES)])
+
+    # Clear progress once done
+    progress_bar.empty()
+    progress_text.empty()
+
+    too_many_links_message = "This page might have too many on-page links." if len(link_elements) > 100 else ""
+
+    gpt_insights = internal_link_analysis_gpt(error_messages, broken_links, very_long_links, access_errors, resource_as_page_links, too_many_links_message)
+
+    result = {
+        "message": gpt_insights,
+        "what_it_is": "Linking checks for the page.",
+        "how_to_fix": "Each link issue is categorized. Review the specific issue category for recommendations.",
+        "audit_name": "Linking Audit"
+    }
+
+    return result
+
 st.title("Single Page SEO Auditor")
 url = st.text_input("Enter URL of the page to audit")
 
 if url:
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
     title_results = TT(url)
     meta_results = MD(url)
     link_results = IL(url)
