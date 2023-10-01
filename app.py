4646707dafa855d237b81bc65408b5f08d175a31
boshify <140970186+boshify@users.noreply.github.com>
2023-09-30 22:08:14 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,15 +7,6 @@ import requests
 def TT(url):
     """
     Checks the title tag of the given page.
-    - Determines if the title tag is missing.
-    - Checks if the title tag's content is too long (more than 60 characters).
-    - Checks if it's too short (less than 10 characters).
-    
-    Parameters:
-    - url (str): The URL of the page to check.
-
-    Returns:
-    - dict: A dictionary with the title content and the result message.
     """
     # Fetch the content of the page
     response = requests.get(url)
@@ -34,30 +25,30 @@ def TT(url):
     
     title_length = len(title_tag.text)
     
-    # Check if title is too long or too short
-    if title_length > 60:
-        return {"title": title_tag.text, "message": f"Fail: Title tag is too long ({title_length} characters)."}
-    elif title_length < 10:
-        return {"title": title_tag.text, "message": f"Fail: Title tag is too short ({title_length} characters)."}
-    
-    return {"title": title_tag.text, "message": "Pass: Title tag is within the recommended length."}
+    # Update to include "What it is" and "How to fix it" information
+    result = {
+        "title": title_tag.text,
+        "message": "",
+        "what_it_is": "The title tag provides a brief summary of the content of the page and is displayed in search engine results and browser tabs.",
+        "how_to_fix": ""
+    }
 
-# <><><><><><><> END OF FUNCTION TT <><><><><><><>
+    if title_length > 60:
+        result["message"] = f"Fail: Title tag is too long ({title_length} characters)."
+        result["how_to_fix"] = "Reduce the length of the title tag to 60 characters or fewer."
+    elif title_length < 10:
+        result["message"] = f"Fail: Title tag is too short ({title_length} characters)."
+        result["how_to_fix"] = "Increase the length of the title tag to at least 10 characters."
+    else:
+        result["message"] = "Pass: Title tag is within the recommended length."
+
+    return result
 
 # <><><><><><><> START OF FUNCTION MD <><><><><><><>
 
 def MD(url):
     """
     Checks the meta description of the given page.
-    - Determines if the meta description is missing.
-    - Checks if the meta description's content is too long (more than 160 characters).
-    - Checks if it's too short (less than 50 characters).
-    
-    Parameters:
-    - url (str): The URL of the page to check.
-
-    Returns:
-    - dict: A dictionary with the meta description content and the result message.
     """
     # Fetch the content of the page
     response = requests.get(url)
@@ -76,26 +67,28 @@ def MD(url):
     
     description_length = len(meta_description['content'])
     
-    # Check if meta description is too long or too short
-    if description_length > 160:
-        return {"description": meta_description['content'], "message": f"Fail: Meta description is too long ({description_length} characters)."}
-    elif description_length < 50:
-        return {"description": meta_description['content'], "message": f"Fail: Meta description is too short ({description_length} characters)."}
-    
-    return {"description": meta_description['content'], "message": "Pass: Meta description is within the recommended length."}
+    # Update to include "What it is" and "How to fix it" information
+    result = {
+        "description": meta_description['content'],
+        "message": "",
+        "what_it_is": "The meta description provides a brief summary of the content of the page and is displayed in search engine results.",
+        "how_to_fix": ""
+    }
 
-# <><><><><><><> END OF FUNCTION MD <><><><><><><>
+    if description_length > 160:
+        result["message"] = f"Fail: Meta description is too long ({description_length} characters)."
+        result["how_to_fix"] = "Reduce the length of the meta description to 160 characters or fewer."
+    elif description_length < 50:
+        result["message"] = f"Fail: Meta description is too short ({description_length} characters)."
+        result["how_to_fix"] = "Increase the length of the meta description to at least 50 characters."
+    else:
+        result["message"] = "Pass: Meta description is within the recommended length."
+
+    return result
+
+# <><><><><><><> RUN AUDITS FUNCTION <><><><><><><>
 
 def run_audits(url):
-    """
-    Runs all available audit functions on the given URL and returns results.
-    
-    Parameters:
-    - url (str): The URL of the page to audit.
-
-    Returns:
-    - list: A list of results from all audit functions.
-    """
     results = []
 
     # Run title tag check
@@ -103,8 +96,6 @@ def run_audits(url):
     
     # Run meta description check
     results.append(MD(url))
-
-    # More audit functions can be added here in the future...
 
     return results
 
@@ -117,12 +108,13 @@ url = st.text_input("Enter URL of the page to audit")
 if url:
     results = run_audits(url)
     for result in results:
+        st.write("---")  # Line break
         if 'title' in result and result["title"]:
             st.subheader("Title Tag Content")
-            st.write(f"```{result['title']}```")
+            st.info(f"```{result['title']}```")
         elif 'description' in result and result["description"]:
             st.subheader("Meta Description Content")
-            st.write(f"```{result['description']}```")
+            st.info(f"```{result['description']}```")
         
         st.subheader("Audit Result")
-        st.markdown(f"**{result['message']}**")
+        st.info(f"**{result['message']}**\n\n*What it is:* {result['what_it_is']}\n\n*How to fix:* {result['how_to_fix']}")
