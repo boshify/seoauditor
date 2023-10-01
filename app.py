ed2ad74932f79b902bbc58ab09c594b57d768363
boshify <140970186+boshify@users.noreply.github.com>
2023-10-01 14:15:07 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,20 +6,6 @@ import openai
 # Initialize OpenAI with API key from Streamlit's secrets
 openai.api_key = st.secrets["openai_api_key"]
 
-# Progress bar messages
-PROGRESS_MESSAGES = [
-    "Casting SEO spells...",
-    "Unleashing digital spiders...",
-    "Diving into meta tags...",
-    "Whispering to web spirits...",
-    "Riding backlink waves...",
-    "Decoding HTML matrix...",
-    "Unraveling web yarn...",
-    "Consulting the algorithm...",
-    "Fetching search wisdom...",
-    "Brewing the SEO potion..."
-]
-
 def get_gpt_insights(prompt):
     messages = [
         {"role": "system", "content": "You are an SEO expert."},
@@ -37,18 +23,7 @@ def TT(url):
     title_tag = soup.find('title')
     result = {
         "title": title_tag.text if title_tag else None,
-        "message": "",
-        "audit_name": "Title Tag Audit"
     }
-
-    if not title_tag:
-        result["message"] = "Fail: Title tag is missing."
-    elif len(title_tag.text) > 60:
-        result["message"] = f"Fail: Title tag is too long ({len(title_tag.text)} characters)."
-    elif len(title_tag.text) < 10:
-        result["message"] = f"Fail: Title tag is too short ({len(title_tag.text)} characters)."
-    else:
-        result["message"] = "Pass: Title tag is within the recommended length."
 
     return result
 
@@ -58,18 +33,7 @@ def MD(url):
     meta_description = soup.find('meta', attrs={"name": "description"})
     result = {
         "description": meta_description['content'] if meta_description else None,
-        "message": "",
-        "audit_name": "Meta Description Audit"
     }
-
-    if not meta_description or not meta_description.get('content'):
-        result["message"] = "Fail: Meta description is missing."
-    elif len(meta_description['content']) > 160:
-        result["message"] = f"Fail: Meta description is too long ({len(meta_description['content'])} characters)."
-    elif len(meta_description['content']) < 50:
-        result["message"] = f"Fail: Meta description is too short ({len(meta_description['content'])} characters)."
-    else:
-        result["message"] = "Pass: Meta description is within the recommended length."
 
     return result
 
@@ -82,41 +46,19 @@ def IL(url):
     link_elements = main_content.find_all(['a', 'img'], recursive=True)
     
     broken_links = []
-    very_long_links = []
-    access_errors = []
-    resource_as_page_links = []
-    
     for element in link_elements:
         link_url = element.get('href' if element.name == 'a' else 'src')
-        
         if not link_url:
             continue
-        if len(link_url) > 200:
-            very_long_links.append(link_url)
-
         try:
             r = requests.get(link_url, allow_redirects=True, timeout=5)
             if r.status_code >= 400:
                 broken_links.append(link_url)
         except requests.RequestException:
-            access_errors.append(link_url)
-
-        if element.name == 'img' and element.parent.name == 'a':
-            resource_as_page_links.append(link_url)
-
-    prompt_content = "\n".join([
-        "*Broken Links:* " + ", ".join(broken_links) if broken_links else "",
-        "*Very Long URLs:* " + ", ".join(very_long_links) if very_long_links else "",
-        "*Access Errors:* " + ", ".join(access_errors) if access_errors else "",
-        "*Resource Images as Links:* " + ", ".join(resource_as_page_links) if resource_as_page_links else ""
-    ])
-
-    prompt = f"Analyze the internal linking audit results and provide a brief summarized analysis and recommendations based on the following data: \n\n{prompt_content}"
-    insights = get_gpt_insights(prompt)
+            pass
 
     return {
-        "message": insights,
-        "audit_name": "Linking Audit"
+        "broken_links": broken_links,
     }
 
 def AnchorText(url):
@@ -127,13 +69,9 @@ def AnchorText(url):
         main_content = soup.body  # Default to body if "main" tag is not found
     anchor_elements = main_content.find_all('a', href=True)
     anchor_texts = [anchor.text.strip() for anchor in anchor_elements if anchor.text.strip()]
-    combined_texts = "\n".join(anchor_texts)
-    prompt = f"Provide an analysis and recommendations based on the following anchor texts: \n\n{combined_texts}"
-    insights = get_gpt_insights(prompt)
 
     return {
-        "message": insights,
-        "audit_name": "Anchor Text Audit"
+        "anchor_texts": anchor_texts,
     }
 
 # Streamlit UI
@@ -141,21 +79,33 @@ url = st.text_input("Enter URL of the page to audit")
 
 if url:
     with st.spinner("Analyzing..."):
+        # Title Tag Analysis
         title_tag_result = TT(url)
-        st.subheader(title_tag_result["audit_name"])
-        st.write("Title Tag Content:", title_tag_result["title"])
-        insights = get_gpt_insights(f"Analyze the title tag '{title_tag_result['title']}' and provide insights and optimization suggestions.")
-        st.markdown(f"**GPT Insights:** {insights}")
-        st.write(title_tag_result["message"])
+        st.subheader("Title Tag Audit")
+        st.markdown(f"**Title Tag Content:** {title_tag_result['title']}")
+        insights_title = get_gpt_insights(f"Provide a concise insight and recommendation for the title tag '{title_tag_result['title']}'.")
+        st.markdown(f"**Insights:** {insights_title}")
 
+        # Meta Description Analysis
         meta_description_result = MD(url)
-        st.subheader(meta_description_result["audit_name"])
-        st.write(meta_description_result["message"])
+        st.subheader("Meta Description Audit")
+        if meta_description_result['description']:
+            st.markdown(f"**Meta Description Content:** {meta_description_result['description']}")
+        else:
+            st.markdown("**Result:** Meta description is missing.")
 
+        # Linking Audit
         linking_result = IL(url)
-        st.subheader(linking_result["audit_name"])
-        st.write(linking_result["message"])
+        st.subheader("Linking Audit")
+        if linking_result['broken_links']:
+            st.markdown("**Broken Links Identified:**")
+            for link in linking_result['broken_links']:
+                st.markdown(f"- {link}")
 
+        # Anchor Text Audit
         anchor_text_result = AnchorText(url)
-        st.subheader(anchor_text_result["audit_name"])
-        st.write(anchor_text_result["message"])
+        st.subheader("Anchor Text Audit")
+        for anchor in anchor_text_result['anchor_texts'][:5]:  # Displaying only first 5 for brevity
+            insights_anchor = get_gpt_insights(f"Provide a concise insight for the anchor text '{anchor}'.")
+            st.markdown(f"**Anchor Text:** {anchor}")
+            st.markdown(f"**Insight:** {insights_anchor}")
