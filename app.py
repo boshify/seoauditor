334ac2b464e1404a02f8a9f1b440a4d28c769200
boshify <140970186+boshify@users.noreply.github.com>
2023-10-01 14:18:57 -0600

    Update app.py

--- app.py
+++ app.py
@@ -19,93 +19,62 @@ def get_gpt_insights(prompt):
 
 def TT(url):
     response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    title_tag = soup.find('title')
-    result = {
-        "title": title_tag.text if title_tag else None,
-    }
-
-    return result
+    soup = BeautifulSoup(response.text, 'html.parser')
+    title = soup.title.string
+    insights = get_gpt_insights(f"Analyze the title tag: {title}")
+    return title, insights
 
 def MD(url):
     response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    meta_description = soup.find('meta', attrs={"name": "description"})
-    result = {
-        "description": meta_description['content'] if meta_description else None,
-    }
-
-    return result
+    soup = BeautifulSoup(response.text, 'html.parser')
+    meta_description = soup.find('meta', attrs={'name': 'description'})
+    if meta_description:
+        return meta_description['content']
+    else:
+        return None
 
 def IL(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    main_content = soup.find('main')  # Assuming "main" tag is used for main content
-    if not main_content:
-        main_content = soup.body  # Default to body if "main" tag is not found
-    link_elements = main_content.find_all(['a', 'img'], recursive=True)
-    
-    broken_links = []
-    for element in link_elements:
-        link_url = element.get('href' if element.name == 'a' else 'src')
-        if not link_url:
-            continue
-        try:
-            r = requests.get(link_url, allow_redirects=True, timeout=5)
-            if r.status_code >= 400:
-                broken_links.append(link_url)
-        except requests.RequestException:
-            pass
-
-    return {
-        "broken_links": broken_links,
-    }
+    # This is a placeholder for your Linking Audit logic
+    # You'd replace the logic here with the actual implementation
+    return "Linking Audit results go here"
 
 def AnchorText(url):
     response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    main_content = soup.find('main')  # Assuming "main" tag is used for main content
-    if not main_content:
-        main_content = soup.body  # Default to body if "main" tag is not found
-    anchor_elements = main_content.find_all('a', href=True)
-    anchor_texts = [anchor.text.strip() for anchor in anchor_elements if anchor.text.strip()]
+    soup = BeautifulSoup(response.text, 'html.parser')
+    anchor_texts = [a.string for a in soup.find_all('a') if a.string]
+    insights = get_gpt_insights(f"Analyze the anchor texts: {', '.join(anchor_texts[:5])} and more")
+    return anchor_texts, insights
 
-    return {
-        "anchor_texts": anchor_texts,
-    }
-
-# Streamlit UI
+st.title("Single Page SEO Auditor")
 url = st.text_input("Enter URL of the page to audit")
 
 if url:
     with st.spinner("Analyzing..."):
-        # Title Tag Analysis
-        title_tag_result = TT(url)
-        st.subheader("Title Tag Audit")
-        st.markdown(f"**Title Tag Content:** {title_tag_result['title']}")
-        insights_title = get_gpt_insights(f"Provide a concise insight and recommendation for the title tag '{title_tag_result['title']}'.")
-        st.markdown(f"**Insights:** {insights_title}")
+        # Title Tag Audit
+        with st.container():
+            st.subheader("🏷️ Title Tag Audit")
+            title, title_insights = TT(url)
+            st.write(f"**Title Tag Content:** {title}")
+            st.write(f"**GPT Insights:** {title_insights}")
 
-        # Meta Description Analysis
-        meta_description_result = MD(url)
-        st.subheader("Meta Description Audit")
-        if meta_description_result['description']:
-            st.markdown(f"**Meta Description Content:** {meta_description_result['description']}")
-        else:
-            st.markdown("**Result:** Meta description is missing.")
+        # Meta Description Audit
+        with st.container():
+            st.subheader("📝 Meta Description Audit")
+            meta_desc = MD(url)
+            if meta_desc:
+                st.write(f"**Meta Description Content:** {meta_desc}")
+            else:
+                st.write("❌ Meta description is missing.")
 
         # Linking Audit
-        linking_result = IL(url)
-        st.subheader("Linking Audit")
-        if linking_result['broken_links']:
-            st.markdown("**Broken Links Identified:**")
-            for link in linking_result['broken_links']:
-                st.markdown(f"- {link}")
+        with st.container():
+            st.subheader("🔗 Linking Audit")
+            linking_results = IL(url)
+            st.write(linking_results)
 
         # Anchor Text Audit
-        anchor_text_result = AnchorText(url)
-        st.subheader("Anchor Text Audit")
-        for anchor in anchor_text_result['anchor_texts'][:5]:  # Displaying only first 5 for brevity
-            insights_anchor = get_gpt_insights(f"Provide a concise insight for the anchor text '{anchor}'.")
-            st.markdown(f"**Anchor Text:** {anchor}")
-            st.markdown(f"**Insight:** {insights_anchor}")
+        with st.container():
+            st.subheader("⚓ Anchor Text Audit")
+            anchor_texts, anchor_insights = AnchorText(url)
+            st.write(f"**Sample Anchor Texts:** {', '.join(anchor_texts[:5])} and more...")
+            st.write(f"**GPT Insights:** {anchor_insights}")
