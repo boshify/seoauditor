a4a0286227e9e364de1e94634e0d5f629ae08171
boshify <140970186+boshify@users.noreply.github.com>
2023-10-01 21:58:10 -0600

    Update app.py

--- app.py
+++ app.py
@@ -82,55 +82,6 @@ def LinkingAudit(url):
         response = request_url(url)
         if not response:
             return [{"issue": "Error fetching URL", "solution": "Failed to retrieve content for linking audit", "example": url}]
-
-        soup = BeautifulSoup(response.text, 'html.parser')
-
-        # Exclude common header, navigation, and footer areas
-        for element in soup.find_all(['header', 'nav', 'footer']):
-            element.extract()
-
-        # If available, focus on the main content area
-        main_content = soup.find('main') or soup.find('article') or soup.find('section') or soup
-
-        structured_issues = []
-        seen_links = set()
-
-        links = main_content.find_all('a', href=True)
-        base_domain = urlparse(url).netloc
-
-        for link in links:
-            href = link['href']
-            full_url = urljoin(url, href)
-
-            # Check if link is internal
-            if base_domain in urlparse(full_url).netloc and full_url not in seen_links and not href.startswith('#'):
-                seen_links.add(full_url)
-                # Now, instead of raising an issue immediately, we'll check if it's a relative or absolute link
-                if not href.startswith(('http://', 'https://')):
-                    issue = f"Relative internal link found: {full_url}"
-                    solution = "Consider making internal links absolute for clarity, although it's not strictly necessary."
-                    structured_issues.append({
-                        "issue": issue,
-                        "solution": solution,
-                        "example": href
-                    })
-
-        if not structured_issues:
-            structured_issues.append({
-                "issue": "No internal links found.",
-                "solution": "Consider adding relevant internal links to improve user navigation and SEO."
-            })
-
-        return structured_issues
-    except Exception as e:
-        return [{"issue": "Unexpected error during linking audit", "solution": str(e), "example": url}]
-
-
-def LinkingAudit(url):
-    try:
-        response = request_url(url)
-        if not response:
-            return [{"issue": "Error fetching URL", "solution": "Failed to retrieve content for linking audit", "example": url}]
         
         soup = BeautifulSoup(response.text, 'html.parser')
         for element in soup.find_all(['header', 'nav', 'footer']):
@@ -148,20 +99,31 @@ def LinkingAudit(url):
             href = link['href']
             full_url = urljoin(url, href)
 
-            if base_domain not in full_url or href.startswith('#') or full_url in seen_links:
-                continue
-            
-            seen_links.add(full_url)
+            # If link is internal and not checked before
+            if base_domain in urlparse(full_url).netloc and full_url not in seen_links and not href.startswith('#'):
+                seen_links.add(full_url)
+                
+                # Check if the link resolves correctly
+                link_response = request_url(full_url)
+                if not link_response or link_response.status_code >= 400:
+                    structured_issues.append({
+                        "issue": f"Broken internal link found: {full_url}",
+                        "solution": f"Ensure the link is pointing to the correct location. Status Code: {link_response.status_code if link_response else 'Failed to fetch'}",
+                        "example": href
+                    })
 
         if not structured_issues:
             structured_issues.append({
-                "issue": "No internal links found.",
-                "solution": "Consider adding relevant internal links to improve user navigation and SEO."
+                "issue": "No internal linking issues found.",
+                "solution": "All internal links on the page seem to be working correctly."
             })
 
         return structured_issues
     except Exception as e:
         return [{"issue": "Unexpected error during linking audit", "solution": str(e), "example": url}]
+
+# The modified LinkingAudit function is defined above. It checks the status code of every internal link and reports those that don't resolve correctly.
+
 
 
 def AnchorTextAudit(url):
