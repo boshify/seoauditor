a4697737247ba75435ce41cd84101f3ae9dff151
boshify <140970186+boshify@users.noreply.github.com>
2023-10-01 22:55:05 -0600

    Update app.py

--- app.py
+++ app.py
@@ -323,68 +323,7 @@ def analyze_pagespeed_data(data):
 
     return crux_metrics, lighthouse_metrics
 
-def ImageAudit_v2(url):
-    response = request_url(url)
-    if not response:
-        return {"error": "Failed to retrieve content for image audit"}
 
-    soup = BeautifulSoup(response.text, 'html.parser')
-    img_elements = soup.find_all('img')
-
-    missing_alt = []
-    existing_alt = []
-    broken_imgs = []
-    non_descriptive_names = []
-
-    base_domain = urlparse(url).netloc
-
-    for img in img_elements:
-        # Check for missing alt attributes
-        if not img.get('alt'):
-            missing_alt.append(urljoin(url, img['src']))
-        else:
-            existing_alt.append((urljoin(url, img['src']), img['alt']))
-
-        # Check for internal broken images
-        img_src = urljoin(url, img['src'])
-        if base_domain in urlparse(img_src).netloc:
-            img_response = request_url(img_src)
-            if not img_response:
-                broken_imgs.append(img_src)
-
-        # Non-descriptive filenames will be processed later (after the loop)
-        img_name = urlparse(img['src']).path.split('/')[-1]
-        if len(img_name.split('-')) <= 1:
-            non_descriptive_names.append(img_src)
-
-    # Recommendations for missing alt attributes
-    alt_recommendations = []
-    for img_src in missing_alt:
-        img_name = urlparse(img_src).path.split('/')[-1]
-        alt_suggestion = get_gpt_insights(f"Suggest an alt text for the image with filename: {img_name}")
-        alt_recommendations.append((img_src, alt_suggestion))
-
-    # Improved alt text for existing alt attributes
-    improved_alt_texts = []
-    for img_src, alt_text in existing_alt:
-        improved_alt_suggestion = get_gpt_insights(f"Suggest a better alt text for the image with current alt text: {alt_text}")
-        improved_alt_texts.append((img_src, improved_alt_suggestion))
-
-    # Improved filenames for non-descriptive names
-    improved_filenames = []
-    for img_src in non_descriptive_names:
-        img_name = urlparse(img_src).path.split('/')[-1]
-        filename_suggestion = get_gpt_insights(f"Suggest a more descriptive filename for the image with current name: {img_name}")
-        improved_filenames.append((img_src, filename_suggestion))
-
-    return {
-        "missing_alt": (missing_alt, "Images should have alt attributes for accessibility and SEO.", alt_recommendations),
-        "existing_alt": (existing_alt, "Checking the descriptiveness of existing alt texts.", improved_alt_texts),
-        "broken_imgs": (broken_imgs, "Broken images can lead to poor user experience.", "Consider re-uploading or fixing the source of the broken images."),
-        "non_descriptive_names": (non_descriptive_names, "Descriptive image filenames can help with image SEO.", improved_filenames)
-    }
-
-# The updated ImageAudit_v2 function is defined above. It provides detailed insights and recommendations based on the findings.
 
 
 
